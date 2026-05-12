6000a2b0e94e425f55da9e4ee82833f3e51386db
Milton.Neto <milton.neto@parnamirim.rn.gov.br>
2026-05-12 15:09:47 -0300

    cria home dashboard

---
+++ frontend/src/app/features/dashboard/components/activity-feed/activity-feed.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-feed',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-feed.html',
+  styleUrl: './activity-feed.css',
+})
+export class ActivityFeed {}
---
+++ frontend/src/app/features/dashboard/components/ranking-list/ranking-list.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ranking-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './ranking-list.html',
+  styleUrl: './ranking-list.css',
+})
+export class RankingList {}
